dea1de7e403c5b13cf022b107a81f40f50aa21a0
floragunn <dev@search-guard.com>
2017-01-13 10:55:35 +0100

    2.4

--- searchguard.sh
+++ searchguard.sh
@@ -17,52 +17,59 @@ do_install() {
   
   echo "Stopping services"
   
-  systemctl stop kibana.service > /dev/null 2>&1
-  systemctl stop metricbeat.service > /dev/null 2>&1
+  #systemctl stop kibana.service > /dev/null 2>&1
+  #systemctl stop metricbeat.service > /dev/null 2>&1
   systemctl stop elasticsearch.service > /dev/null 2>&1
   
   echo "Install packages"
   
-  ES_VERSION=5.0.2
+  ES_VERSION=2.4.1
   
-  if [ ! -f "elasticsearch-$ES_VERSION.deb" ]; then
-    wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION.deb > /dev/null 2>&1
-    check_ret
-  fi
+  #if [ ! -f "elasticsearch-$ES_VERSION.deb" ]; then
+  #  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION.deb > /dev/null 2>&1
+  #  check_ret
+  #fi
+  
+  https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.4.1/elasticsearch-2.4.1.deb
+
   
   dpkg --force-all -i elasticsearch-$ES_VERSION.deb > /dev/null 2>&1
   check_ret
   
-  wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-$ES_VERSION-amd64.deb > /dev/null 2>&1
-  check_ret
+  #wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-$ES_VERSION-amd64.deb > /dev/null 2>&1
+  #check_ret
   
-  dpkg --force-all -i metricbeat-$ES_VERSION-amd64.deb > /dev/null 2>&1
-  check_ret
+  #dpkg --force-all -i metricbeat-$ES_VERSION-amd64.deb > /dev/null 2>&1
+  #check_ret
   
-  wget https://artifacts.elastic.co/downloads/kibana/kibana-$ES_VERSION-amd64.deb > /dev/null 2>&1
-  check_ret
+  #wget https://artifacts.elastic.co/downloads/kibana/kibana-$ES_VERSION-amd64.deb > /dev/null 2>&1
+  #check_ret
   
-  dpkg --force-all -i kibana-$ES_VERSION-amd64.deb > /dev/null 2>&1
-  check_ret
+  #dpkg --force-all -i kibana-$ES_VERSION-amd64.deb > /dev/null 2>&1
+  #check_ret
   
-  NETTY_NATIVE_VERSION=1.1.33.Fork23
+  NETTY_NATIVE_VERSION=1.1.33.Fork27
   NETTY_NATIVE_CLASSIFIER=linux-x86_64
   ES_BIN=/usr/share/elasticsearch/bin
   ES_CONF=/etc/elasticsearch
   ES_LOG=/var/log/elasticsearch
   ES_PLUGINS=/usr/share/elasticsearch/plugins
   SG_VERSION=$ES_VERSION-9
+  SG_SSL_VERSION=$ES_VERSION-19
   ORG_NAME="Example DSG Inc. 1.0"
   
   echo "SG_PUBHOST: $SG_PUBHOST"
   echo "SG_PRIVHOST: $SG_PRIVHOST"
   
-  $ES_BIN/elasticsearch-plugin remove discovery-ec2 > /dev/null 2>&1
-  $ES_BIN/elasticsearch-plugin remove search-guard-5 > /dev/null 2>&1
+  $ES_BIN/plugin remove discovery-ec2 > /dev/null 2>&1
+  $ES_BIN/plugin remove search-guard-2 > /dev/null 2>&1
+  $ES_BIN/plugin remove search-guard-ssl > /dev/null 2>&1
   
-  $ES_BIN/elasticsearch-plugin install -b discovery-ec2 > /dev/null 
+  $ES_BIN/plugin install -b discovery-ec2 > /dev/null 
   check_ret
-  $ES_BIN/elasticsearch-plugin install -b com.floragunn:search-guard-5:$SG_VERSION > /dev/null 
+  $ES_BIN/plugin install -b com.floragunn:search-guard-2:$SG_VERSION > /dev/null 
+  check_ret
+  $ES_BIN/plugin install -b com.floragunn:search-guard-ssl:$SG_SSL_VERSION > /dev/null 
   check_ret
   
   cd /demo_root_ca
@@ -100,27 +107,27 @@ do_install() {
   
   if [ ! -f "netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar" ]; then
     echo "Download netty native"
-    wget -O $ES_PLUGINS/search-guard-5/netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar https://search.maven.org/remotecontent?filepath=io/netty/netty-tcnative/$NETTY_NATIVE_VERSION/netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar > /dev/null 2>&1
+    wget -O $ES_PLUGINS/search-guard-ssl/netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar https://search.maven.org/remotecontent?filepath=io/netty/netty-tcnative/$NETTY_NATIVE_VERSION/netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar > /dev/null 2>&1
     check_ret
   fi
   
   cd - > /dev/null 2>&1
   
-  cd $ES_PLUGINS/search-guard-5
+  cd $ES_PLUGINS/search-guard-2
 
   echo "Download modules"
 
-  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-authbackend-ldap&v=5.0-5" --content-disposition  > /dev/null 2>&1
+  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-authbackend-ldap&v=2.4-6" --content-disposition  > /dev/null 2>&1
   check_ret
-  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-module-auditlog&v=5.0-3" --content-disposition  > /dev/null 2>&1
+  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-module-auditlog&v=2.4-3" --content-disposition  > /dev/null 2>&1
   check_ret
-  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-rest-api&v=5.0-3" --content-disposition  > /dev/null 2>&1
+  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-rest-api&v=2.4-3" --content-disposition  > /dev/null 2>&1
   check_ret
-  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-auth-http-kerberos&v=5.0-2" --content-disposition  > /dev/null 2>&1
+  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-auth-http-kerberos&v=2.4-2" --content-disposition  > /dev/null 2>&1
   check_ret
-  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-auth-http-jwt&v=5.0-2" --content-disposition  > /dev/null 2>&1
+  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-auth-http-jwt&v=2.4-2" --content-disposition  > /dev/null 2>&1
   check_ret
-  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-module-dlsfls&v=5.0-5" --content-disposition  > /dev/null 2>&1
+  wget "http://oss.sonatype.org/service/local/artifact/maven/content?c=jar-with-dependencies&r=releases&g=com.floragunn&a=dlic-search-guard-module-dlsfls&v=2.4-5" --content-disposition  > /dev/null 2>&1
   check_ret
   cd - > /dev/null 2>&1
   
@@ -154,7 +161,7 @@ do_install() {
      echo "node.data: false" >> $ES_CONF/elasticsearch.yml
   fi
   
-  echo "node.ingest: false" >> $ES_CONF/elasticsearch.yml
+  #echo "node.ingest: false" >> $ES_CONF/elasticsearch.yml
   
   echo "cluster.routing.allocation.disk.watermark.high: 10mb" >> $ES_CONF/elasticsearch.yml
   echo "cluster.routing.allocation.disk.watermark.low: 10mb" >> $ES_CONF/elasticsearch.yml
@@ -212,12 +219,13 @@ do_install() {
   if [[ $SG_PRIVHOST == *"10-0-0-12"* ]]; then
   # coord
   
-      sleep 50
+      echo "wait ..."
+      sleep 120
   
 	  post_slack "run sgadmin $SG_PUBHOST $SG_PRIVHOST"
   
-	  chmod +x $ES_PLUGINS/search-guard-5/tools/sgadmin.sh
-	  $ES_PLUGINS/search-guard-5/tools/sgadmin.sh -cd /demo_root_ca/sgconfig -h $SG_PRIVHOST -icl -ts $ES_CONF/truststore.jks -ks $ES_CONF/CN=$SG_PRIVHOST-keystore.jks
+	  chmod +x $ES_PLUGINS/search-guard-2/tools/sgadmin.sh
+	  $ES_PLUGINS/search-guard-2/tools/sgadmin.sh -cd /demo_root_ca/sgconfig -h $SG_PRIVHOST -icl -ts $ES_CONF/truststore.jks -ks $ES_CONF/CN=$SG_PRIVHOST-keystore.jks
 	  check_ret
 	  post_slack "SG $SG_VERSION initialized on https://$SG_PUBHOST:9200"
   
@@ -237,23 +245,23 @@ do_install() {
 		"message" : "rockn roll"
 	  }'
 
-	  cat /demo_root_ca/kibana/kibana.yml | sed -e "s/RPLC_HOST/$SG_PUBHOST/g" > /etc/kibana/kibana.yml 
-	  echo 'searchguard.cookie.password: "a12345678912345678912345678912345678987654c"' >> /etc/kibana/kibana.yml 
-	  /usr/share/kibana/bin/kibana-plugin install https://files.slack.com/files-pri/T0KUZ3JGN-F3JC0QZ38/download/searchguard-kibana-5.0.2.zip?pub_secret=39500fd32d
+	  #cat /demo_root_ca/kibana/kibana.yml | sed -e "s/RPLC_HOST/$SG_PUBHOST/g" > /etc/kibana/kibana.yml 
+	  #echo 'searchguard.cookie.password: "a12345678912345678912345678912345678987654c"' >> /etc/kibana/kibana.yml 
+	  #/usr/share/kibana/bin/kibana-plugin install https://files.slack.com/files-pri/T0KUZ3JGN-F3JC0QZ38/download/searchguard-kibana-5.0.2.zip?pub_secret=39500fd32d
 
-	  /bin/systemctl enable kibana.service
-	  check_ret
-	  systemctl start kibana.service  
-	  check_ret
+	  #/bin/systemctl enable kibana.service
+	  #check_ret
+	  #systemctl start kibana.service  
+	  #check_ret
   
-	  cat /demo_root_ca/metricbeat/metricbeat.yml | sed -e "s/RPLC_HOST/$SG_PUBHOST/g" > /etc/metricbeat/metricbeat.yml
+	  #cat /demo_root_ca/metricbeat/metricbeat.yml | sed -e "s/RPLC_HOST/$SG_PUBHOST/g" > /etc/metricbeat/metricbeat.yml
 
-	  /bin/systemctl enable metricbeat.service
-	  check_ret
-	  systemctl start metricbeat.service
-	  check_ret
+	  #/bin/systemctl enable metricbeat.service
+	  #check_ret
+	  #systemctl start metricbeat.service
+	  #check_ret
   
-	  post_slack "Kibana $SG_VERSION running on https://$SG_PUBHOST:5601"
+	  #post_slack "Kibana $SG_VERSION running on https://$SG_PUBHOST:5601"
 	
 	  curl -ksS -u admin:admin "https://$SG_PUBHOST:9200/_cluster/health?pretty" > health 2>&1
 	  check_ret
